ba5ed8e78cabe0e442453843d56bd21a0b13a01f
david.ferrari <david.ferrari@primeurad.com>
2024-10-01 12:11:14 +0200

    Step 1: Introduced Main
    - Added Main class to take input from user and check if it is a valid empty json

---
+++ src/main/java/org/jitpack/practice/Main.java
@@ -0,0 +1,18 @@
+package org.jitpack.practice;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String input = null;
+        try (var keyboard = new Scanner(System.in)){
+            input = keyboard.nextLine();
+            var parser = new Parser(new Lexer(input));
+            parser.parse();
+            System.out.println("Json Object " + input + " is valid.");
+        } catch (InvalidSyntaxException | UnexpectedTokenException e) {
+            System.err.println("Json Object " + input + " is invalid.");
+            System.err.println(e.getMessage());
+        }
+    }
+}
